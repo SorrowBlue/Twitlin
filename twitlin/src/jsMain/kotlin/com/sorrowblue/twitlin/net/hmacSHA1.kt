37430f54189c03c72957911341b34c830120e671
SorrowBlue <sorrowblue.dev@gmail.com>
2020-08-05 07:02:16 +0900

    * js hmacSHA1 created

--- twitlin/src/jsMain/kotlin/com/sorrowblue/twitlin/net/hmacSHA1.kt
+++ twitlin/src/jsMain/kotlin/com/sorrowblue/twitlin/net/hmacSHA1.kt
@@ -1,12 +1,17 @@
 package com.sorrowblue.twitlin.net
 
-external fun HmacSHA1(value: String, key: String): String
+@JsModule("crypto-js/hmac-sha1")
+@JsNonModule
+private external fun HmacSHA1(value: String, key: String): String
 
-external class Base64 {
+@JsModule("crypto-js/enc-base64")
+@JsNonModule
+private external class Base64 {
 	companion object {
 		fun stringify(str: String): String
 	}
 }
 
-internal actual fun hmacSHA1(key: ByteArray, value: ByteArray): String =
-	Base64.stringify(HmacSHA1(value.decodeToString(), key.decodeToString())) as String
+internal actual fun hmacSHA1(key: ByteArray, value: ByteArray): String {
+	return Base64.stringify(HmacSHA1(value.decodeToString(), key.decodeToString()))
+}
