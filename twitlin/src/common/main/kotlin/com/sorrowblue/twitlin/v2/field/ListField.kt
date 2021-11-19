66a055fb5f3bf761caf50ab4931340c938f1cd40
SorrowBlue <sorrowblue.dev@gmail.com>
2021-11-19 20:26:40 +0900

    Added description to ListField.

--- twitlin/src/common/main/kotlin/com/sorrowblue/twitlin/v2/field/ListField.kt
+++ twitlin/src/common/main/kotlin/com/sorrowblue/twitlin/v2/field/ListField.kt
@@ -4,14 +4,16 @@ public enum class ListField(override val value: String) : OptionalField {
     CREATED_AT("created_at"),
     FOLLOWER_COUNT("follower_count"),
     MEMBER_COUNT("member_count"),
-    PRIVATE("private");
+    PRIVATE("private"),
+    DESCRIPTION("description");
 
     public companion object {
         public fun all(): List<ListField> = listOf(
             CREATED_AT,
             FOLLOWER_COUNT,
             MEMBER_COUNT,
-            PRIVATE
+            PRIVATE,
+            DESCRIPTION
         )
     }
 }
